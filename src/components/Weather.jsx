22da35517a5d87a1cc1ff8f9fab21eb721164266
RahulR458 <rahul404dev@gmail.com>
2024-04-03 00:23:53 +0530

    updated css and structure

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,16 +5,32 @@ export default function Weather() {
   return (
     <>
       <div  className="weather">
-        <input type="text" id="search-name"/>
         <div  className="weather-container">
-        Weather
-        <div id="temperature">temperature</div>
-        <div id='presipitation'>presipitation</div>
-        <div id='humidity'>Humidity</div>
-        <div id='visibility'>visibility</div>
+        <form action="" className='weather__search'>
+          <input type="text" className="search-name"/>
+        </form>
+          <div  className="weather__info">
+            <div>
+              <h1>23 &deg;C,</h1> 
+              Mostly Cloudy
+            </div>
+          </div>
         </div>
-        <div className=''></div>
+        <div  className="weather-icons">
+          <div className='climate-group-one'>
+            <div className='climate'>temperature</div>
+            <div className='climate'>presipitation</div>
+            <div className='climate'>hhh</div>
+          </div>
+          <div className='climate-group-two'>
+            <div className='climate'>Humidity</div>
+            <div className='climate'>visibility</div>
+            <div className='climate'>hhshs</div>
+          </div>
+          <div className='weather-box'></div> 
+        </div>
       </div>
+      
     </>
   )
 }
