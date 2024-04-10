8bae4482ef93f8e9268a7d6df06f1e4fd41d239f
RahulR458 <rahul404dev@gmail.com>
2024-04-10 23:57:17 +0530

    api integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,75 @@
 import { useState } from 'react'
 import Weather from './components/Weather'
+import { useEffect } from 'react';
+import axios from 'axios';
 
 
 function App() {
 
+  const [weatherSearch, setWeatherSearch] = useState("India");
+  const [weather, setWeather] = useState({});
+  const [weatherLocation, setWeatherLocation] = useState({});
+  const [weatherCurrentDate, setWeatherCurrentDate] = useState("");
+  const [weatherCurrentTime, setWeatherCurrentTime] = useState([]);
+
+  const search = (event) => {
+    setWeatherSearch(event.target.value);
+  };
+
+  const searchSubmit = async (event) => {
+    event.preventDefault();
+    const response = await axios(
+        `http://api.weatherapi.com/v1/current.json?key=542e3558a43d4510b3e173051240104&q=${weatherSearch}&aqi=yes`
+    );
+    setWeather(response.data.current);
+    setWeatherLocation(response.data.location);
+  };
+
+  const getweather = async () => {
+    const response = await axios(
+        `http://api.weatherapi.com/v1/current.json?key=542e3558a43d4510b3e173051240104&q=INDIA&aqi=yes`
+    );
+
+    let dateTimeString = response.data.current.last_updated;
+
+    // Split the date and time
+    let parts = dateTimeString.split(" ");
+    let datePart = parts[0];
+    let timePart = parts[1];
+    let parts2 = timePart.split(":");
+    let datePart2 = parts2[0];
+
+    const responses = await axios(
+        `http://api.weatherapi.com/v1/history.json?key=542e3558a43d4510b3e173051240104&q=INDIA&dt=${datePart}&aqi=yes`
+    );
+
+    let result = responses.data.forecast.forecastday[0].hour;
+    let filterResult = result.filter((data, index) => index >= datePart2);
+    let filterResultOg = filterResult.filter((data, index) => index < 5);
+    console.log(filterResultOg);
+    setWeatherCurrentTime(filterResultOg);
+
+    setWeatherCurrentDate(response.data.current.last_updated);
+
+    setWeather(response.data.current);
+    setWeatherLocation(response.data.location);
+  };
+
+  useEffect(()=>{
+    getweather()
+  },[])
+
   return (
-    <>
-      <p >
-        {/* Click on the Vite and React logos to learn more */}
-        < Weather />
-      </p>
-    </>
-  )
+    <Weather
+      weatherSearch={weatherSearch}
+      weather={weather}
+      weatherLocation={weatherLocation}
+      weatherCurrentDate={weatherCurrentDate}
+      weatherCurrentTime={weatherCurrentTime}
+      search={search}
+      searchSubmit={searchSubmit}
+    />
+  );
 }
 
 export default App
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import './Weather.css'
 
-export default function Weather() {
+export default function Weather({weatherSearch, weather, weatherLocation, weatherCurrentDate, weatherCurrentTime, search, searchSubmit}) {
+
   return (
     <>
       <div  className="weather">
         <div  className="weather-container">
           <div>
-            <form action="" className='weather__search'>
+            <form action="" onSubmit={searchSubmit} className='weather__search'>
             <span className="fa-solid fa-location-dot"></span>
-              <input type="text" className="search-name" />
+              <input type="text" className="search-name" onChange={search} value={weatherSearch}/>
             </form>
           </div>
           <div className="weather__info">
+            <div className='weather-icon-png'><img src="/public/weather-icon-png.webp" alt="" /></div>
             <div className='text-weather-info'>
-              <h1>23 &deg;C,</h1> 
+              <h1>{weather.temp_c} &deg;C,</h1> 
               Mostly Cloudy
+              <h1>{weather.condition?.text}</h1>
+              <img src={weather.condition?.icon} alt="" />
             </div>
           </div>
         </div>
@@ -23,28 +27,30 @@ export default function Weather() {
           <div className='climate-box-one'>
             <div className='text-climate '><i className="fa-solid fa-clock"></i><h2>HOURLY FORECAST</h2></div>
             <div className='climate-group-one'>
-              <div className='climate'><h5>Now</h5><h3>28 &deg;</h3><i class="fa-solid fa-cloud"></i></div>
-              <div className='climate'></div>
+              <div className='climate'><h5>Now</h5><h3>{weather.temp_c} &deg;</h3><i className="fa-solid fa-cloud"></i></div>
+              {weatherCurrentTime.map((data, index)=>(
+                <div className='climate' key={index}><h5>date</h5><h3>{data.temp_c} &deg;</h3><i className="fa-solid fa-cloud"></i></div>
+              ))}
             </div>
           </div>
           <div className='climate-box-two'>
-            <div className='text-climate'><i class="fa-solid fa-calendar-week"></i><h2>7 DAYS FORECAST</h2></div>
+            <div className='text-climate'><i className="fa-solid fa-calendar-week"></i><h2>7 DAYS FORECAST</h2></div>
             <div className='climate-group-two'>
-              <div className='climate'><h5>Now</h5><h6>09/05</h6><h3>28 &deg;</h3><i class="fa-solid fa-cloud"></i></div>
+              <div className='climate'><h5>Now</h5><h6>09/05</h6><h3>28 &deg;</h3><i className="fa-solid fa-cloud"></i></div>
               <div className='climate'></div>
             </div>
           </div>
           <div className='weather-box-container'>
-            <div className='text-highlights'><i class="fa-solid fa-calendar-day"></i><h1>Today's Highlights</h1></div>
+            <div className='text-highlights'><i className="fa-solid fa-calendar-day"></i><h1>Today's Highlights</h1></div>
             <div className='weather-group'>
-              <div className='weather-box'><i class="fa-solid fa-temperature-three-quarters"></i>FEELS LIKE</div>
-              <div className='weather-box'><i class="fa-solid fa-snowflake"></i>PRECIPITATION</div>
-              <div className='weather-box'><i class="fa-solid fa-eye-slash"></i>VISIBILIYU</div>  
+              <div className='weather-box'><i className="fa-solid fa-temperature-three-quarters"></i>FEELS LIKE <br />{weather.feelslike_c}&deg;</div>
+              <div className='weather-box'><i className="fa-solid fa-snowflake"></i>PRECIPITATION <br />{weather.precip_in}''</div>
+              <div className='weather-box'><i className="fa-solid fa-eye-slash"></i>VISIBILIYU <br />{weather.vis_miles}mi</div>  
             </div>
             <div className='weather-group'>
-              <div className='weather-box'><i class="fa-solid fa-droplet"></i>HUMIDITY</div>
-              <div className='weather-box'><i class="fa-solid fa-temperature-arrow-down"></i>UV INDEX</div>
-              <div className='weather-box'><i class="fa-solid fa-wind"></i>WIND</div>
+              <div className='weather-box'><i className="fa-solid fa-droplet"></i>HUMIDITY <br />{weather.humidity}%</div>
+              <div className='weather-box'><i className="fa-solid fa-temperature-arrow-down"></i>UV INDEX <br />{weather.uv}&deg;</div>
+              <div className='weather-box'><i className="fa-solid fa-wind"></i>WIND <br />{weather.wind_mph}MPH</div>
             </div>
           </div> 
         </div>
